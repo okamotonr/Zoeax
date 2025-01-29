2a1bf624bb164ad544dc8585d0818939be9dca09
okamotonr <r.okamoton@gmail.com>
2025-01-29 17:58:18 +0900

    start point: accessibility

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -5,16 +5,16 @@
 #![allow(clippy::missing_safety_doc)]
 
 mod address;
-pub mod capability;
+mod capability;
 pub mod common;
 pub mod handler;
 pub mod init;
-pub mod ipc_args;
+mod ipc_args;
 mod memlayout;
-pub mod object;
-pub mod riscv;
-pub mod sbi;
-pub mod scheduler;
-pub mod syscall;
-pub mod timer;
+mod object;
+mod riscv;
+mod sbi;
+mod scheduler;
+mod syscall;
+mod timer;
 pub mod uart;
